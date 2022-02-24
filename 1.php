faae6616efacaa5b07d6e2f3587e97e6cd464032
Artur <70844316+arcziosppl@users.noreply.github.com>
2022-02-24 20:59:28 +0100

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+
+<div class="d1">
+<?php
+
+
+$day = date("d-m-Y H-i-s");
+
+echo "do pliku aktualny.txt dopisałeś " .  $day;
+
+$dane_zapis = $day . "\n";
+
+
+$file = fopen("aktualny.txt", "a+") or die("nie można otworzyć pliku");
+
+fwrite($file, $dane_zapis);
+
+?>
+</div>
+    
+</body>
+</html>
